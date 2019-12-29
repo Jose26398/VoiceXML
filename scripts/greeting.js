08383fc9cc9dabb077e5a8994d52058cb828283c
Vladislav Nikolov <vladis890@gmail.com>
2019-12-30 00:02:14 +0100

    Terminada version en español e ingles

---
+++ scripts/greeting.js
@@ -0,0 +1,29 @@
+function greetEng(){
+    var d = new Date();
+    var time = d.getHours();
+
+    if (time < 12) {
+        return "Good morning";
+    }
+    if (time < 17) {
+        return"Good afternoon.";
+    }
+    if (time >= 19) {
+        return "Good night";
+    }
+}
+
+function greetEsp(){
+    var d = new Date();
+    var time = d.getHours();
+
+    if (time < 12) {
+        return "Buenos dias";
+    }
+    if (time < 17) {
+        return"Buenas tardes";
+    }
+    if (time >= 19) {
+        return "Buenas noches";
+    }
+}
